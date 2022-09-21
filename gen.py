ce5c37df5ff7db0745827741a9a42f3b97babfd9
Piotr Szymajda <PiotrSzymajda@10g.pl>
2022-09-21 21:59:52 +0200

    Format code and add customizable dates for identifiers

--- gen.py
+++ gen.py
@@ -1,39 +1,44 @@
-from jinja2 import Environment, FileSystemLoader
 import difflib
+import json
+import yaml
 
-NUMBER_OF_IDENTIFIERS = 230
+from jinja2 import Environment, FileSystemLoader
 
+
+NUMBER_OF_BLANK_IDENTIFIERS = 20
+ZOSIA_DATE = "02.03.2023 - 05.03.2023"
 HIGHLIGHTED_ORGANIZATIONS = {
     "FINGO": "gold",
     "Ten Square Games": "gold",
     "Antmicro": "gold"
 }
 
+env = Environment(loader=FileSystemLoader('./'))
+
+
 def load_yaml_file(path):
-    import yaml
-    with open(path, "r") as f:
-        file_content = f.read()
+    with open(path, "r", encoding="utf-8") as file:
+        file_content = file.read()
         file_data = yaml.load(file_content, Loader=yaml.Loader)
         return file_data
 
+
 def load_json_file(path):
-    import json
-    with open(path, "r") as f:
-        file_content = f.read()
+    with open(path, "r", encoding="utf-8") as file:
+        file_content = file.read()
         file_data = json.loads(file_content)
         return file_data
 
-schedule = load_yaml_file("schedule.yaml");
-places = load_yaml_file("places.yaml");
-data = load_json_file("data.json");
 
 def write_to_file(path, content):
-    with open(path, "w") as f:
-        f.write(content)
+    with open(path, "w", encoding="utf-8") as file:
+        file.write(content)
+
 
 def render(template, context):
     template = env.get_template(template)
     return template.render(context)
+
 
 def make_indetifier_context(data):
     prefs = []
@@ -51,7 +56,7 @@ def make_indetifier_context(data):
             "breakfast_4": p["breakfast_day_4"],
         })
 
-    blank_identifiers = NUMBER_OF_IDENTIFIERS - len(prefs)
+    blank_identifiers = NUMBER_OF_BLANK_IDENTIFIERS
     for _ in range(blank_identifiers):
         prefs.append({
             "first_name": "..............",
@@ -64,24 +69,27 @@ def make_indetifier_context(data):
             "dinner_4": True,
             "breakfast_4": True,
         })
-    
-    return {"prefs": prefs}
+
+    return {"prefs": prefs, "camp_date": ZOSIA_DATE}
+
 
 def gen_identifier(data):
     context = make_indetifier_context(data)
     rendered = render("identifier/identifier_template.html", context)
     write_to_file("gen/identifier.html", rendered)
 
+
 def get_lecture_with_close_title(title, data):
-    titles = [l["title"] for l in data["lectures"]]
-    best_title = difflib.get_close_matches(title, titles);
+    titles = [lecture["title"] for lecture in data["lectures"]]
+    best_title = difflib.get_close_matches(title, titles)
     best_title_index = titles.index(best_title[0])
     return data["lectures"][best_title_index]
 
-def calculate_end_time(startTime, duration):
+
+def calculate_end_time(start_time, duration):
     if duration == 0:
         return ""
-    x = startTime.split(':')
+    x = start_time.split(':')
     m = (int(x[1]) + duration)
     h = int(x[0]) + m // 60
     m = m % 60
@@ -89,6 +97,7 @@ def calculate_end_time(startTime, duration):
     m = str(m) if len(str(m)) == 2 else "0" + str(m)
     h = str(h) if len(str(h)) == 2 else "0" + str(h)
     return str(h) + ":" + str(m)
+
 
 def combine_schedule_event_and_data(schedule_event, data):
     end_time = calculate_end_time(schedule_event["startTime"], schedule_event["duration"])
@@ -107,6 +116,7 @@ def combine_schedule_event_and_data(schedule_event, data):
         "organization": lecture_data["author__preferences__organization__name"],
     }
 
+
 def combine_schedule_and_data(schedule, data):
     result = []
     for day in schedule:
@@ -118,24 +128,30 @@ def combine_schedule_and_data(schedule, data):
         })
     return result
 
+
 def gen_book_and_schedule(schedule, data):
     days = combine_schedule_and_data(schedule, data)
 
     book_template = env.get_template('book/book_template.html')
-    string=book_template.render({"days": days, "places": places})
-    with open("gen/book.html", "w") as text_file:
+    string = book_template.render({"days": days, "places": places})
+    with open("gen/book.html", "w", encoding="utf-8") as text_file:
         text_file.write(string)
 
     schedule_template_html = env.get_template('schedule/schedule_template.html')
-    string=schedule_template_html.render({"days": days})
-    with open("gen/schedule.html", "w") as text_file:
-        text_file.write(string);
+    string = schedule_template_html.render({"days": days})
+    with open("gen/schedule.html", "w", encoding="utf-8") as text_file:
+        text_file.write(string)
 
     schedule_template_html = env.get_template('schedule/web_schedule_template.html')
-    string=schedule_template_html.render({"days": days})
-    with open("gen/web_schedule.html", "w") as text_file:
-        text_file.write(string);
-    
-env = Environment(loader=FileSystemLoader('./'))
-gen_identifier(data)
-gen_book_and_schedule(schedule, data)
+    string = schedule_template_html.render({"days": days})
+    with open("gen/web_schedule.html", "w", encoding="utf-8") as text_file:
+        text_file.write(string)
+
+
+if __name__ == "__main__":
+    schedule = load_yaml_file("schedule.yaml")
+    places = load_yaml_file("places.yaml")
+    data = load_json_file("data.json")
+
+    gen_identifier(data)
+    gen_book_and_schedule(schedule, data)
