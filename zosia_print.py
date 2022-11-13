84b5f9d42b963f147d28fe4bc79bfc2fcce15911
Piotr Szymajda <PiotrSzymajda@10g.pl>
2022-11-13 21:58:45 +0100

    Render all target files

--- zosia_print.py
+++ zosia_print.py
@@ -1,14 +1,23 @@
 import os
 import json
+import locale
 import argparse
 
 from datetime import date
-from typing import List, Any
+from typing import List, Dict, Any, Optional
 
 import yaml
 
+from jinja2 import Environment, FileSystemLoader
+from weasyprint import HTML
+from weasyprint.text.fonts import FontConfiguration
+
+
 PLACES_PATH = "./data/places"
 SCHEDULES_PATH = "./data/schedules"
+TEMPLATES_PATH = "./templates"
+TARGET_DIR = "./gen"
+env = Environment(loader=FileSystemLoader(TEMPLATES_PATH))
 
 
 def load_yaml_file(path: str) -> Any:
@@ -25,6 +34,20 @@ def load_json_file(path: str) -> Any:
 
 def get_options(path: str) -> List[str]:
     return [f.rstrip(".yaml") for f in os.listdir(path)]
+
+
+def render_template(template: str, context: Dict[str, Any]) -> Any:
+    return env.get_template(template).render(context)
+
+
+def render_pdf(
+        path: str, ctx: Dict[str, Any], name: Optional[str] = None) -> None:
+    template_dir = path.rsplit('/', 1)[0]
+    target_name = name if name is not None else template_dir
+    HTML(
+        string=render_template(path, ctx),
+        base_url=f"{TEMPLATES_PATH}/{template_dir}"
+    ).write_pdf(f"{TARGET_DIR}/{target_name}.pdf")
 
 
 def main() -> None:
@@ -46,13 +69,58 @@ def main() -> None:
     print(args)
 
     schedule = load_yaml_file(f"{SCHEDULES_PATH}/{args.schedule}.yaml")
-    places = load_yaml_file(f"{PLACES_PATH}/{args.place}.yaml")
+    place = load_yaml_file(f"{PLACES_PATH}/{args.place}.yaml")
     data = load_json_file(args.data)
 
     # TODO: Validate data
     # TODO: Load information about highlighted organizations
     # and contacts to organizers
-    # TODO: Generate PDF files ;)
+
+    # Make sure that target directory exist
+    os.makedirs(TARGET_DIR, exist_ok=True)
+
+    # Enforce polish locale to generate proper month name
+    locale.setlocale(locale.LC_ALL, 'pl_PL.utf8')
+    start_date = date.fromisoformat(data["zosia"]["start_date"])
+    end_date = date.fromisoformat(data["zosia"]["end_date"])
+    zosia_date = f"{start_date:%-d} - {end_date:%-d %B %Y}"
+    if start_date.month != end_date.month:
+        zosia_date = f"{start_date:%-d %B} - {end_date:%-d %B %Y}"
+
+    contacts = {
+      "Aneta Kos": "111 222 333",
+      "Jan Kowalski": "222 333 444",
+      "Bartosz Kurek": "123 456 789",
+    }
+
+    # TODO: Render day-by-day schedule:
+    days_schedule = []
+
+    # Book
+    render_pdf("book/book_template.html", {
+        "days": days_schedule,
+        "place": place,
+        "contacts": contacts,
+        "camp_date": zosia_date
+    })
+
+    # Schedule
+    render_pdf("schedule/schedule_template.html", {
+        "days": days_schedule
+    })
+
+    # Web schedule for zosia-site
+    with open(f"{TARGET_DIR}/web_schedule.html", "w", encoding="utf-8") as file:
+        file.write(render_template(
+            "schedule/web_schedule_template.html", {
+                "days": days_schedule
+            }))
+
+    # Identifier
+    render_pdf("identifier/identifier_template.html", {
+        "prefs": [],
+        "camp_date": zosia_date
+    })
 
 
 if __name__ == "__main__":
